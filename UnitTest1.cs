3a5813953f8b9e70df87b843d46a77c1aa2f0ae1
sergiopires <sergiocop1@hotmail.com>
2015-03-23 23:26:39 -0300

    First app

---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Treino.Aluno.DomainModel;
+using Treino.Aluno.Repository;
+using System.Data.Entity;
+
+using Treino.Aluno.UI.Web;
+namespace TESTE
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SexoREP sexo = new SexoREP();
+
+            sexo.Criar();
+        }
+
+         [TestMethod]
+        public void TESTAR_CLASSIFICACAO()
+        {
+
+           
+
+        }
+
+
+
+    }
+}
